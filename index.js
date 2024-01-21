892205b8ad43adcad445015d46c3e4868afd5ffc
Taylor Glenn <taylor.glenn@ampp.org>
2024-01-21 13:19:25 -0600

    can't use an import statement outside of a module.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from 'express'; //Import the express dependency
+const express = require('express'); //Import the express dependency
 const app = express();              //Instantiate an express app, the main work horse of this server
 const port = 5000;                  //Save the port number where your server will be listening
 
